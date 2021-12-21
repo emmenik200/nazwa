591769b3978ad4a0cc7e5e754cee97e3acae83cb
Bartek <bartchol20@gmail.com>
2021-12-21 13:27:07 +0100

    lepiej

---
+++ java.js
@@ -0,0 +1,19 @@
+var words = ['piłka nożna'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
